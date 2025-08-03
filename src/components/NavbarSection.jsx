35bcdafbe01a4936ad403d07914590b27f5ccd13
Githubshakil <shakilmahmud007@gmail.com>
2025-08-03 18:38:01 +0600

    update
    Responsive Navbar for Mobile

--- src/components/NavbarSection.jsx
+++ src/components/NavbarSection.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { FaShoppingBag } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars, FaShoppingBag } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 import { Link, NavLink } from "react-router-dom";
 
 const navItems = [
@@ -9,11 +10,11 @@ const navItems = [
   { name: "Contact", path: "/contact" },
 ];
 
-const NavItems = () => {
+const NavItems = ({toggleMenu}) => {
   return (
-    <ul className="flex flex-col md:flex-row items-center md:space-x-8 gap-8 font-poppins text-[18px] text-[#171616]">
+    <ul className="flex flex-col md:flex-row items-center md:space-x-8 gap-8 font-poppins text-[18px]">
       {navItems.map((item, index) => (
-        <li key={index}>
+        <li key={index} onClick={ toggleMenu}>
           <NavLink
             to={item.path}
             className={({ isActive }) =>
@@ -30,6 +31,11 @@ const NavItems = () => {
   );
 };
 const NavbarSection = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // Placeholder for future state management
+
+    const toggleMenu = () => {
+        setIsMenuOpen(prevState => !isMenuOpen);
+    };
   return (
     <header>
       <nav className="max-w-screen-2xl container mx-auto flex justify-between items-center py-6 px-4 ">
@@ -37,6 +43,23 @@ const NavbarSection = () => {
         <Link to={"/"} className="font-bold font-poppins text-2xl">
           Logo
         </Link>
+
+
+        {/* hamburger menu for mobile */}
+        <div onClick={toggleMenu} className="md:hidden cursor-pointer text-xl hover:text-primary transition duration-300 ">
+            {
+                isMenuOpen ? null : <FaBars />
+            }
+            
+        </div>
+
+        {/* mobile menu items */}
+        <div className={`fixed top-0 left-0 w-full h-screen bg-black/80 text-white flex flex-col items-center justify-center space-y-8 transition-transform duration-300 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"} md:hidden`}>
+            <div className="cursor-pointer absolute top-4 right-5 text-xl" onClick={toggleMenu}>
+                <IoClose />
+            </div>
+            <NavItems toggleMenu={toggleMenu}/>
+        </div>
 
         {/*Desktop  menu items */}
         <div className="hidden md:block">
