04de5e71e155a58955711eafb97862c2089bdc04
Githubshakil <shakilmahmud007@gmail.com>
2025-08-04 03:09:51 +0600

    update

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import btnIcon from '../assets/images/button-icon.png'
-const Button = ({text}) => {
+const ButtonComponent = ({text}) => {
   return (
-     <button className="text-sm text-primary flex items-center gap-1 font-poppins">
+    <button className="text-sm text-primary flex items-center gap-1 font-poppins">
       {text}
       <img src={btnIcon} alt="" />
     </button>
   )
 }
 
-export default Button
+export default ButtonComponent
