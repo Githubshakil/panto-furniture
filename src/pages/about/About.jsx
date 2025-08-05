3e3cffb7c7f7efbe39bf0fb89bb962b2af6264fb
Githubshakil <shakilmahmud007@gmail.com>
2025-08-05 13:19:41 +0600

    update
    about page & Contact Page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import aboutBgImg from '../../assets/images/about-background.jpg'
+import Expriences from '../home/Expriences'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section>
+        {/* banner */}
+        <div className='w-full h-[400px] bg-no-repeat bg-cover flex items-center justify-center text-white' style={{backgroundImage:`url(${aboutBgImg})`}}>
+          <div>
+            <h1 className='text-5xl font-poppins font-bold underline-offset-4'>About Page</h1>
+          </div>
+        </div>
+        <Expriences/>
+    </section>
   )
 }
 
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import contactBgImg from '../../assets/images/contact-background.jpg'
+import Materials from '../home/Materials'
+import Testimonial from '../home/Testimonial'
 const Contact = () => {
   return (
-    <div>Contact</div>
+     <section>
+        {/* banner */}
+        <div className='w-full h-[400px] bg-no-repeat bg-cover flex items-center justify-center text-white' style={{backgroundImage:`url(${contactBgImg})`}}>
+          <div>
+            <h1 className='text-5xl font-poppins font-bold underline-offset-4'>Contact Page</h1>
+          </div>
+        </div>
+        <Materials/>
+        <Testimonial/>
+    </section>
   )
 }
 
